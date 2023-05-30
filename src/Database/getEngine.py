1f6991a8d25af49fd2da6712a1ad93d6deee02df
kian-parvaneh <kianp32@gmail.com>
2023-05-30 17:38:13 +0330

    DB,validation,controllers should be complete

--- src/Database/getEngine.py
+++ src/Database/getEngine.py
@@ -1,13 +1,9 @@
-import os;
+from dotenv import dotenv_values;
 from sqlalchemy import create_engine;
 from sqlalchemy.ext.declarative import declarative_base;
 from sqlalchemy.orm import sessionmaker;
-
-
-MYSQL_HOSTNAME = os.getenv("MYSQL_HOSTNAME");
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD");
-MYSQL_DATABASE = os.getenv("MYSQL_DATABASE");
-db_engine = create_engine(f"mysql+mysqlconnector://<root>:{MYSQL_PASSWORD}@{MYSQL_HOSTNAME}:3306/{MYSQL_DATABASE}",echo=True);
+env = dotenv_values('../.env');
+db_engine = create_engine(f"mysql+mysqlconnector://root:{env['MYSQL_PASSWORD']}@{env['MYSQL_HOSTNAME']}:3306/{env['MYSQL_DATABASE']}",echo=True);
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine);
--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
+from Database.getEngine import SessionLocal, db_engine;
+from Database.Models import models
+app = FastAPI();
 
-app = FastAPI()
+models.Base.metadata.create_all(bind=db_engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 
